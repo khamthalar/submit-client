b779f1f6b61324f87d3a6b96a932b55a9b107e8d
khamthalar <tikkhamthalar@gmail.com>
2019-07-05 17:26:51 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule,MatMenuModule,MatCardModule,MatSidenavModule } from '@angular/material';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule,MatMenuModule,MatCardModule,MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { ToggleComponent } from './components/toggle/toggle.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    ToggleComponent
+    ToggleComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +32,10 @@ import { ToggleComponent } from './components/toggle/toggle.component';
     MatSelectModule,
     MatMenuModule,
     MatCardModule,
-    MatSidenavModule
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
