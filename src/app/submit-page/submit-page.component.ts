10ef0f46560c86822ac8a1df34cb7fd4d3a1e7d4
khamthalar <tikkhamthalar@gmail.com>
2019-08-21 14:02:20 +0700

    git commit

--- src/app/submit-page/submit-page.component.ts
+++ src/app/submit-page/submit-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder,FormControl } from '@angular/forms';
+import { MatRadioChange } from '@angular/material';
 
 @Component({
   selector: 'app-submit-page',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-page.component.css']
 })
 export class SubmitPageComponent implements OnInit {
-
-  constructor() { }
-
+  submitForm:FormGroup;
+  test:String;
+  rd_select:boolean=false;
+  constructor(private router:Router,fb: FormBuilder) { 
+    this.submitForm = fb.group({
+      rd_device:new FormControl(''),
+      txt_rd:new FormControl(''),
+      txt_des:new FormControl(''),
+      rd_choices:new FormControl('')
+    })
+  }
   ngOnInit() {
   }
+  back_Clicked(){
+    // window.alert(this.submitForm.value.txt_rd);
+    this.router.navigate(['home']);
+  }
+  rd_changed(event:MatRadioChange){
+    if(event.source.id=="rd03"){
+      this.rd_select = true;
+    }else{
+      this.rd_select = false;
+    }
 
+  }
 }
