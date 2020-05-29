e528179d28e3f4ef8c3fd5095564bc3d8ecdb32e
Khamthalar5566 <tikkhamthalar@gmail.com>
2020-05-29 22:46:21 +0700

    change project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Login002
+# request_fix
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'login002'`, () => {
+  it(`should have as title 'request_fix'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('login002');
+    expect(app.title).toEqual('request_fix');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to login002!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to request_fix!');
   });
 });
